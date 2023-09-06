f2d12036500bce17db08ea5378ae7e09c79cfe73
StefanoMela <stefano.mela25@gmail.com>
2023-09-06 15:18:11 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,47 @@
+
+
+axios
+    .get('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then((response) => {
+
+        console.log(response);
+        console.log(response.data);
+        console.log(response.data.response);
+});
+
+
+
+
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+
+            emailsArray: "",
+
+        }
+    },
+
+    methods: {
+
+        getEmails() {
+
+            for (let i = 0; i < 10; i++) {
+
+                axios
+                    .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((response) => {
+
+                        const result = response.data.response;
+
+                        console.log(result + " il cui indice è :" + [i]);
+
+                        console.log(this.emailsArray);
+                    });
+
+            };
+        },
+    },
+
+}).mount('#app')
