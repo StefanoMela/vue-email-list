452353710d5a09600b9e1c8153ad14b9f971dc5c
StefanoMela <stefano.mela25@gmail.com>
2023-09-06 15:41:12 +0200

    feat: bonus

--- js/main.js
+++ js/main.js
@@ -1,16 +1,11 @@
+axios
+    .get('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then((response) => {
 
-
-// axios
-//     .get('https://flynn.boolean.careers/exercises/api/random/mail')
-//     .then((response) => {
-
-//         console.log(response);
-//         console.log(response.data);
-//         console.log(response.data.response);
-//     });
-
-
-
+        console.log(response);
+        console.log(response.data);
+        console.log(response.data.response);
+});
 
 const { createApp } = Vue
 
@@ -46,7 +41,7 @@ createApp({
         },
     },
 
-    mounted () {
+    mounted() {
 
         this.getEmails();
     }
